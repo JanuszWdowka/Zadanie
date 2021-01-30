e4139f392e65ecb85c92c7ee6284c78752a88c42
JW <janusz_2000@hotmail.com>
2021-01-30 09:36:05 +0100

    2.0

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 
 int main() {
     printf("Hello, World!\n");
-    printf("poprawiona v1.1");
+    printf("poprawiona v2.0");
     printf("new option 1");
     printf("new option 2");
     return 0;
